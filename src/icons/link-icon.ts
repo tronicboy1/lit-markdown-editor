a5b120e931f13be823734c3e3d4f817ee29b61cb
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-09-29 12:55:31 +0900

    fix icon import names

--- src/icons/link-icon.ts
+++ src/icons/link-icon.ts
@@ -1,6 +1,6 @@
 import { html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
-import iconStyles from "../styles/icon";
+import iconStyles from "../styles/icon.js";
 
 export const tagName = "link-icon";
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,22 @@
 import { LitElement, PropertyValueMap } from "lit";
 export declare const tagName = "lit-markdown-editor";
+/**
+ * A markdown editor text area built using Lit
+ *
+ * Usage in lit:
+ * @example import "lit-markdown-editor"
+ * ...
+ * render() {
+ *    return html`<lit-markdown-editor name="markdown" id="markdown"></lit-markdown-editor>`
+ * }
+ *
+ * Adding event listeners:
+ * @example
+ * <script>
+ * const litMarkdownEditorElement = document.querySelector("lit-markdown-editor");
+ * litMarkdownEditorElement.addEventListener("input", () => console.log(litMarkdownEditorElement.value))
+ * </script>
+ */
 export declare class LitMarkdownEditor extends LitElement {
     #private;
     name: string;
@@ -17,4 +34,9 @@ export declare class LitMarkdownEditor extends LitElement {
     static styles: import("lit").CSSResult[];
     renderToLightDom(): void;
     render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "lit-markdown-editor": LitMarkdownEditor;
+    }
 }
