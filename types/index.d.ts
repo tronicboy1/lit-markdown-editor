3eab3a4d353f9fb45c7fd47840406e99c9da140a
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-09-30 11:57:50 +0900

    commit build

--- types/index.d.ts
+++ types/index.d.ts
@@ -23,21 +23,55 @@ export declare class LitMarkdownEditor extends LitElement {
     required: boolean;
     protected textarea: HTMLTextAreaElement;
     protected fileInput: HTMLInputElement;
+    /**
+     * Acts as an intermediate for this element to behave like a textarea.
+     */
     get value(): string;
     set value(value: string);
     constructor();
     connectedCallback(): void;
     protected firstUpdated(_changedProperties: PropertyValueMap<unknown> | Map<PropertyKey, unknown>): void;
     disconnectedCallback(): void;
+    /**
+     * Renders header Markdown symbols into the textarea.
+     */
     protected handleHeaderClick: EventListener;
+    /**
+     * Handles a click to modifiers like italics.
+     * The textarea selection will be set to the middle of symbols.
+     */
     protected handleModifierClick: EventListener;
+    /**
+     * Renders templates when a template icon is clicked.
+     */
     protected handleTemplateClick: EventListener;
+    /**
+     * Opens file selection dialogue on a click to the add picture icon.
+     */
     protected handleAddPictureClick: EventListener;
+    /**
+     * Handles file input when add picture button is clicked
+     */
     protected handleFileInput: EventListener;
+    /**
+     * Handles a file drop on the textarea element.
+     * Can render multiple files at once.
+     */
     protected handleDrop: (event: DragEvent) => void;
+    /**
+     * Renders a file to the text area as markdown text.
+     * By default, this function will also register the image as an Object URL so it may be displayed in an img tag.
+     */
     protected handleFileRender: (file: File) => void;
+    /**
+     * Triggers input event on button clicks.
+     */
     protected triggerInputEvent(): void;
     static styles: import("lit").CSSResult[];
+    /**
+     * Renders a hidden textarea to the lightdom so this element can be used with forms.
+     * Will use element internals later.
+     */
     renderToLightDom(): void;
     render(): import("lit-html").TemplateResult<1>;
 }
