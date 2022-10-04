3ac2fea88bad83a34f88fcbbeca50dd86b60d6e7
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-05 08:52:00 +0900

    fix loading icon viewport settings

--- test-env/vite.config.ts
+++ test-env/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   build: {
     lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
+      entry: "src/my-element.ts",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: /^lit/
-    }
-  }
-})
+      external: /^lit/,
+    },
+  },
+});
