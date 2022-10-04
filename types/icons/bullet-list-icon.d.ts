e300731aeed6c199bcf409d430a8479ba678da03
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-04 12:04:35 +0900

    add ol and ul list buttons

---
+++ types/icons/bullet-list-icon.d.ts
@@ -0,0 +1,6 @@
+import { LitElement } from "lit";
+export declare const tagName = "bullet-list-icon";
+export declare class BulletListIcon extends LitElement {
+    static styles: import("lit").CSSResult[];
+    render(): import("lit-html").TemplateResult<1>;
+}
---
+++ types/icons/number-list-icon.d.ts
@@ -0,0 +1,6 @@
+import { LitElement } from "lit";
+export declare const tagName = "number-list-icon";
+export declare class NumberListIcon extends LitElement {
+    static styles: import("lit").CSSResult[];
+    render(): import("lit-html").TemplateResult<1>;
+}
